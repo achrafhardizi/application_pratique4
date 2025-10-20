eac36119d38a0ee2c402a2e787e84178a24d3207
achrafhardizi <achrafhardizi1002@gmail.com>
2025-10-20 14:47:45 +0100

    feat: added keynote service along with exposing rest repository controller and seeded the database

---
+++ conference-svc/src/main/java/com/enset/conferencesvc/ConferenceSvcApplication.java
@@ -0,0 +1,13 @@
+package com.enset.conferencesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceSvcApplication.class, args);
+    }
+
+}
---
+++ conference-svc/src/test/java/com/enset/conferencesvc/ConferenceSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enset.conferencesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
